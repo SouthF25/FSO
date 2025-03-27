00facef1f19c7038e0a1c554b4022fe05627451c
SouthF25 <franklysouth@gmail.com>
2025-03-27 14:25:11 -0400

    Refactored the Content component so that it renders three Part components

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -7,14 +7,25 @@ const Header = (props) => {
   );
 };
 
-// Content Component
-const Content = (props) => {
+// Part Component
+const Part = (props) => {
   return (
     <>
       <p>
-        {props.part} {props.exercise}
+        {props.part} {props.exercises}
       </p>
     </>
+  );
+};
+
+// Content Component
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   );
 };
 
@@ -42,9 +53,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
